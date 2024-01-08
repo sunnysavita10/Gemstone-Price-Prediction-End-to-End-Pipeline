b7d76fc8a6daca10f0c2c158a8524fb7e2268b91
sunnysavita10 <engsunny3365@gmail.com>
2024-01-08 20:56:35 +0530

    latest comit

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPEN_E_DOT='-e .'
+
+'''def get_requiremet(file_path:str)->List[str]:
+    requirements = []
+    with open(file_path) as f:
+        requirements=f.readlines()
+        requirements=[req.replace("\n","")for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements'''
+
+   
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()     
+   
+
+__version__ = "0.0.4"
+REPO_NAME = "mongodb_connector"
+PKG_NAME= "MongoDB-Connect"
+AUTHOR_USER_NAME = "sunnysavita10"
+AUTHOR_EMAIL = "sunny.savita@ineuron.ai"
+
+setup(
+    name=PKG_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A python package for connecting with database.",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    
+    
+)
--- template.py
+++ template.py
@@ -1,34 +1,23 @@
 import os
 from pathlib import Path
 
+package_name = "mongodb_connect"
 
-list_of_files=[
-
-    ".github/workflows/.gitkeep",
-    "src/__init__.py",
-    "src/components/__init__.py",
-    "src/components/data_ingestion.py",
-    "src/components/data_transformation.py",
-    "src/components/model_trainer.py",
-    "src/components/model_evaluation.py",
-    "src/pipeline/__init__.py",
-    "src/pipeline/training_pipeline.py",
-    "src/pipeline/prediction_pipeline.py",
-    "src/utils/__init__.py",
-    "src/utils/utils.py",
-    "src/logger/logging.py",
-    "src/exception/exception.py",
-    "tests/unit/__init__.py",
-    "tests/integration/__init__.py",
-    "init_setup.sh",
-    "requirements.txt",
-    "requirements_dev.txt",
-    "setup.py",
-    "setup.cfg",
-    "pyproject.toml",
-    "tox.ini",
-    "experiment/experiments.ipynb"
-
+list_of_files = [
+   ".github/workflows/ci.yaml",
+   "src/__init__.py",
+   f"src/{package_name}/__init__.py", 
+   f"src/{package_name}/mongo_crud.py", 
+   "tests/__init__.py",
+   "tests/unit/__init__.py",
+   "tests/integration/__init__.py",
+   "init_setup.sh",
+   "requirements.txt", 
+   "setup.py",
+   "setup.cfg",
+   "pyproject.toml",
+   "tox.ini",
+   "experiments/experiments.ipynb", 
 ]
 
 for filepath in list_of_files:
@@ -40,3 +29,5 @@ for filepath in list_of_files:
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
             pass # create an empty file
+
+#its updated
