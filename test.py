4000af1a98ca94ddb0cff49a838282162314bdb7
sunnysavita10 <engsunny3365@gmail.com>
2024-01-10 15:59:41 +0530

    logger and exception updated

---
+++ test.py
@@ -0,0 +1,11 @@
+from src.logger.logging import logging
+
+logging.info("this is myb testing")
+
+
+logging.info(" this my second tesgting")
+logging.info(" this my second tesgting")
+
+logging.info(" this my second tesgting")
+logging.info(" this my second tesgting")
+logging.info(" this my second tesgting")
