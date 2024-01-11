74e43f7743042deb3d6fd5e1db831356d90f8cb1
sunnysavita10 <engsunny3365@gmail.com>
2024-01-11 15:47:49 +0530

    predictiofile updated

--- src/pipeline/prediction_pipeline.py
+++ src/pipeline/prediction_pipeline.py
@@ -4,3 +4,34 @@ import pandas as pandas
 from src.exception.exception import customexception
 from src.logger.logging import logging
 from src.utils.utils import load_object
+
+
+class PredictPipeline:
+
+    
+    def __init__(self):
+        print("init.. the object")
+
+    def predict(self,features):
+        try:
+            preprocessor_path=os.path.join("artifacts","preprocessor.pkl")
+            model_path=os.path.join("artifacts","model.pkl")
+
+            preprocessor=load_object(preprocessor_path)
+            model=load_object(model_path)
+
+            scaled_fea=preprocessor.transform(features)
+            pred=model.predict(scaled_fea)
+
+            return pred
+
+        except Exception as e:
+            raise customexception(e,sys)
+
+
+class CustomData:
+    def __init__(self):
+        pass
+
+    def get_data_as_dataframe(self):
+        pass
