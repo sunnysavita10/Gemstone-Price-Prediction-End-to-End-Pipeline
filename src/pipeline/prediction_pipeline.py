dbe57e2f5211a6de50a5af06821326f73793614b
sunnysavita10 <engsunny3365@gmail.com>
2024-01-11 16:34:57 +0530

    app created

--- src/pipeline/prediction_pipeline.py
+++ src/pipeline/prediction_pipeline.py
@@ -1,6 +1,6 @@
 import os
 import sys
-import pandas as pandas
+import pandas as pd
 from src.exception.exception import customexception
 from src.logger.logging import logging
 from src.utils.utils import load_object
@@ -30,8 +30,43 @@ class PredictPipeline:
 
 
 class CustomData:
-    def __init__(self):
-        pass
-
+    def __init__(self,
+                 carat:float,
+                 depth:float,
+                 table:float,
+                 x:float,
+                 y:float,
+                 z:float,
+                 cut:str,
+                 color:str,
+                 clarity:str):
+        
+        self.carat=carat
+        self.depth=depth
+        self.table=table
+        self.x=x
+        self.y=y
+        self.z=z
+        self.cut = cut
+        self.color = color
+        self.clarity = clarity
+            
     def get_data_as_dataframe(self):
-        pass
+        try:
+            custom_data_input_dict = {
+                'carat':[self.carat],
+                'depth':[self.depth],
+                'table':[self.table],
+                'x':[self.x],
+                'y':[self.y],
+                'z':[self.z],
+                'cut':[self.cut],
+                'color':[self.color],
+                'clarity':[self.clarity]
+                }
+            df = pd.DataFrame(custom_data_input_dict)
+            logging.info('Dataframe Gathered')
+            return df
+        except Exception as e:
+            logging.info('Exception Occured in prediction pipeline')
+            raise customexception(e,sys)
