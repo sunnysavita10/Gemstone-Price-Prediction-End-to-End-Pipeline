c44392f5be66b00d6c40af5ffae73c71627070f1
prashant <prashantkhobragade011@gmail.com>
2024-01-11 13:55:24 +0530

    template file corrected

--- template.py
+++ template.py
@@ -1,18 +1,28 @@
 import os
 from pathlib import Path
 
-package_name = "mongodb_connect"
 
 list_of_files = [
    ".github/workflows/ci.yaml",
    "src/__init__.py",
-   f"src/{package_name}/__init__.py", 
-   f"src/{package_name}/mongo_crud.py", 
+   "src/components/__init__.py",
+   "src/components/data_ingestion.py",
+   "src/components/data_transformation.py",
+   "src/components/model_trainer.py",
+   "src/components/model_evaluation.py",
+   "src/pipeline/__init__.py", 
+   "src/pipeline/training_pipeline.py", 
+   "src/pipeline/prediction_pipeline.py",
+   "src/utils/__init__.py",
+   "src/utils/utils.py",
+   "src/logger/logging.py",
+   "src/exception/exeption.py", 
    "tests/__init__.py",
    "tests/unit/__init__.py",
    "tests/integration/__init__.py",
    "init_setup.sh",
-   "requirements.txt", 
+   "requirements.txt",
+   "requirements_dev.txt", 
    "setup.py",
    "setup.cfg",
    "pyproject.toml",
