283198210547ff6ddcd566a2a4adc2f349fc8724
Aryan Agrahari <57763333+aryan757@users.noreply.github.com>
2024-04-10 20:48:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return render_template("index.html")
+    return render_template("form.html")
 
 @app.route("/predict",methods=["GET","POST"])
 def predict_datapoint():
